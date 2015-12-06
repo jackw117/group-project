b9befdf079541dbbe459714edbdd0585750bc9d6
unknown <iguest@is-m342l23-09.ischool.uw.edu>
2015-12-05 19:29:32 -0800

    made some changes to events controller

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Create app
-var myApp = angular.module('myApp', ['ui.router', 'firebase'])
+var myApp = angular.module('myApp', ['ui.router', 'firebase', 'ui.calendar'])
 
 // Configure app
 myApp.config(function($stateProvider, $urlRouterProvider){
@@ -44,14 +44,78 @@ myApp.config(function($stateProvider, $urlRouterProvider){
 })
 
 // Content controller: define $scope.url as an image
-.controller('eventsCtrl', function($scope, $firebaseAuth, $firebaseArray, $firebaseObject){
+.controller('eventsCtrl', function($scope, $firebaseAuth, $firebaseArray, $firebaseObject, $compile, uiCalendarConfig){
 	var ref = new Firebase("https://oca.firebaseio.com/");
 	var usersRef = ref.child("users");
+    var eventsRef = ref.child("events");
 	$scope.users = $firebaseObject(usersRef);
+    $scope.events = $firebaseArray(eventsRef);
 	$scope.authObj = $firebaseAuth(ref);
 	var authData = $scope.authObj.$getAuth();
+    
+    $scope.signInClick = false;
+    $scope.loggedIn = false;
+    $scope.eventClick = false;
+    
+//    $scope.signUp = function() {
+//        $scope.authObj.$createUser({
+//            email: $scope.email,
+//            password: $scope.password
+//        })
+//        .then($scope.logIn())
+//        .then(function(authData) {
+//            $scope.userId = authData.uid;
+//            $scope.users[authData.uid] ={
+//                handle: $scope.email   
+//            }
+//            $scope.users.$save();
+//            
+//        })
+//    }
+    
+    $scope.addEvent = function() {
+        
+        $scope.events.$add({
+            date: $scope.date
+        })
+        .then(function() {
+           $scope.date = ""; 
+        });
+        $scope.eventClick = false;
+        console.log($scope.events)
+    }
 
-
+    $scope.logIn = function() {
+        $scope.signInClick = false;
+        $scope.loggedIn = true;
+        return $scope.authObj.$authWithPassword({
+            email: $scope.email,
+            password: $scope.password
+        });
+    }
+    
+    $scope.signIn = function() {
+        $scope.logIn().then(function(authData){
+            $scope.userId = authData.uid;
+        });
+    }
+    
+//    $scope.checkNames = function() {
+//        $scope.nameInUse = false;
+//        $scope.users.forEach(function(data) {
+//            if (data.handle === $scope.handle) {
+//                $scope.nameInUse = true;
+//            }
+//        });
+//        $scope.checkInUse();
+//    }
+//
+//    $scope.checkInUse = function() {
+//        if (!$scope.nameInUse) {
+//            $scope.signUp();
+//            $scope.signInClick = false;
+//        }
+//    }
 })
 
 // Content controller: define $scope.url as an image
